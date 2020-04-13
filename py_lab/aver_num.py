9bb09ba26ac538f9facc77dc9a77b3d00f09bb86
YH <xoals128@naver.com>
2020-04-13 17:57:21 +0900

    aver_num.py

--- py_lab/aver_num.py
+++ py_lab/aver_num.py
@@ -1,1 +1,10 @@
+#!/usr/bin/python
+sum=0
+n=int(input("input: "))
+for i in range (0,n):
+	num=float(input("%dth score: "%(i+1)))
+	sum=sum+num
+avr=sum/n
+print("average: %f"%avr)
+
 
--- py_lab/fibo_num.py
+++ py_lab/fibo_num.py
@@ -1,1 +1,14 @@
+#!/usr/bin/python
+
+def fib(n):
+	if n == 1 or n == 2:
+		return 1
+	else:
+		return fib(n-1)+fib(n-2)		
+n=int(input("input: "))
+
+for i in range(1,n+1):
+	print("%d "%fib(i))
+print("F%d = %d"%(n, fib(n)))
+	
 
